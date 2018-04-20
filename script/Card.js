e970fcda42f4762cc2eaddddeb1021bfeea30445
Ilya Belavusau <ilyaibelousov@gmail.com>
2018-04-20 13:03:00 +0300

    feature: initialize game

---
+++ script/Card.js
@@ -0,0 +1,42 @@
+function Card(frontCords, skirtCords, index){
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-index', index);
+    let cardFace = document.createElement('div');
+    cardFace.classList.add('front');
+    cardFace.style.background = 'url(images/cards.jpg)' + frontCords;
+    card.appendChild(cardFace);
+    let cardBack = document.createElement('div');
+    cardBack.classList.add('back');
+    cardBack.style.background = 'url(images/cards.jpg) ' + skirtCords;
+    card.appendChild(cardBack);
+    this.card = card;
+}
+
+Card.prototype.rotate = function () {
+    this.classList.add('rotate');
+    this.classList.add('flip');
+};
+
+Card.prototype.rotateBack = function() {
+    this.forEach(function(node) {
+        node.classList.remove('rotate');
+    })
+};
+
+Card.prototype.hide = function() {
+    this.forEach(function(node) {
+        node.lastChild.classList.add('backHide');
+        node.classList.add('match');
+    })
+};
+
+Card.prototype.clearFlip = function() {
+    this.forEach(function(node) {
+        node.classList.remove('flip');
+    })
+};
+
+Card.prototype.isMatch = function () {
+    return this[0].dataset.index === this[1].dataset.index
+};
---
+++ script/game.js
@@ -0,0 +1,152 @@
+function onLoad() {
+    let prevChoice = {};
+    let playerMoves = 0;
+    let playTime;
+    let activeCards = [];
+    let playField = document.getElementsByClassName('playField')[0];
+    let mainScreen = document.getElementsByClassName('mainScreen')[0];
+    let endGame = document.getElementsByClassName('winner')[0];
+    let movesCounter = document.getElementsByClassName('moves')[0];
+    let timer = document.getElementsByClassName('timer')[0];
+    let gameOptionsBtn = document.getElementsByClassName('showLeftMenu')[0];
+    let winnerSection = document.getElementsByClassName('winner');
+
+    document.getElementById('gameStart').addEventListener('click', gameInit);
+    endGame.addEventListener('click', resetGame);
+    gameOptionsBtn.addEventListener('click', showOptions);
+    playField.addEventListener('click',onClickBoard);
+
+    function getParams(value) {
+        let elements = document.getElementsByName(value);
+        let param;
+        elements.forEach(function(item){
+            if (item.checked) {
+                param = item.getAttribute('value');
+            }
+        });
+        return param;
+    }
+
+    function getShuffledArray(params) {
+        let ret = [];
+        for (let i = 0; i < params / 2; i++) {
+            ret.push(i, i);
+        }
+        return ret.sort(() => Math.random() - 0.5);
+    }
+
+
+    function showOptions() {
+        let element = document.getElementsByClassName('options');
+        element[0].classList.toggle('optionsShow')
+    }
+
+    function printNumbersInterval() {
+        let sec = 0;
+        let min = 0;
+        return function () {
+            sec++;
+            if (sec === 60) {
+                min++;
+                sec = 0;
+            }
+            if (min < 10 && min[0] != '0') {
+                min = "0" + min;
+            }
+            if (sec < 10) {
+                sec = "0" + sec;
+            }
+            timer.innerHTML = min + ":" + sec;
+        };
+    }
+
+    function comparePairs(cards) {
+        if (Card.prototype.isMatch.call(cards)) {
+            Card.prototype.hide.call(cards);
+            let cardsCount = getParams('difficult');
+            if (document.getElementsByClassName('match').length == cardsCount) {
+                document.getElementsByClassName('endMoves')[0].innerHTML = movesCounter.innerHTML;
+                document.getElementsByClassName('endTime')[0].innerHTML = timer.innerHTML;
+                winnerSection[0].classList.remove('hide');
+                winnerSection[0].classList.add('show');
+                document.getElementsByClassName('againYou')[0].innerHTML = 'Maybe want one wore round?';
+                clearInterval(playTime);
+                playerMoves = 0;
+            }
+        }
+        else {
+            Card.prototype.rotateBack.call(cards);
+        }
+        Card.prototype.clearFlip.call(cards);
+        prevChoice = {};
+    }
+
+    function onClickCardBack(card) {
+        if(prevChoice !== card){
+            prevChoice = card;
+            playerMoves++;
+            if (playerMoves === 1) {
+                playTime = setInterval(printNumbersInterval(), 1000);
+            }
+            movesCounter.innerHTML = playerMoves;
+            Card.prototype.rotate.call(card);
+            setTimeout(function () {
+                activeCards.push(card);
+                if (activeCards.length === 2) {
+                    comparePairs(activeCards);
+                    activeCards = [];
+                }
+            }, 500)
+        }
+    }
+
+    function changeSections(hideSection, showSection) {
+        hideSection.classList.add('hide');
+        hideSection.classList.remove('show');
+        showSection.classList.remove('hide');
+        showSection.classList.add('show');
+    }
+
+    function resetGame() {
+        if (document.getElementById('boardCreated')) {
+            playField.removeChild(document.getElementById('boardCreated'));
+        }
+        changeSections(playField, mainScreen);
+        endGame.classList.add('hide');
+        endGame.classList.remove('show');
+        movesCounter.innerHTML = '0';
+        timer.innerHTML = '00:00';
+        playerMoves = 0;
+        clearInterval(playTime);
+        prevChoice = {};
+    }
+
+    function gameInit() {
+        resetGame();
+        let difficultValue = getParams('difficult');
+        let skirtsCords = choiceArray[getParams('cardSkirts')];
+        let boardStyle = choiceArray[difficultValue];
+        let frontCords = choiceArray[getParams('cardFronts')];
+        let arr = getShuffledArray(difficultValue);
+        let fragment = document.createDocumentFragment();
+        let board = document.createElement('div');
+        board.classList.add('board');
+        board.id = 'boardCreated';
+        board.classList.add(boardStyle);
+        for (let i = 0; i < difficultValue; i++) {
+            let newCard = new Card(frontCords[arr[i]], skirtsCords, arr[i], board);
+            board.appendChild(newCard.card);
+        }
+        fragment.appendChild(board);
+        playField.appendChild(fragment);
+        changeSections(mainScreen, playField);
+    }
+
+    function onClickBoard(event) {
+        if(event.path[1].classList.contains('card')){
+            onClickCardBack(event.path[1]);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', onLoad());
